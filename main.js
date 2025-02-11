6aebca02034cecc537d4fe967e62bb9ebb8cce2b
EHiz0n <hizon.e-@obmontessori.edu.ph>
2025-02-11 09:00:10 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+function greet(name) {
+    var name = document.getElementById('name').value;
+    document.getElementById('greeting').innerHTML = 'Good evening ' + name + '. You will be loved unconditionally.';
+}
